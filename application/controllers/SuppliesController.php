dcb61fdc4cfd4a0864fbc559b5c5d280a6d4c3f2
Jason Kolenosky <JasonK58@gmail.com>
2016-12-09 15:34:58 -0800

    Pulled my head out of my ass and did a proper job with the model and controller.
    Signed-off-by: Jason Kolenosky <JasonK58@gmail.com>

--- application/controllers/SuppliesController.php
+++ application/controllers/SuppliesController.php
@@ -1,5 +1,4 @@
 <?php
-
 require APPPATH . '/third_party/restful/libraries/Rest_controller.php';
 
 class SuppliesController extends Rest_Controller {
@@ -9,74 +8,146 @@ class SuppliesController extends Rest_Controller {
         $this->load->model('SuppliesModel');
     }
 
-    // Handle an incoming GET ... return a supply item or all of them
     function index_get() {
-        $key = $this->get('code');
-        if (!$key) {
-            $this->response($this->suppliesmodel->all(), 200);
-        } else {
-            $result = $this->suppliesmodel->get($key);
-            if ($result != null)
-                $this->response($result, 200);
-            else
-                $this->response(array('error' => 'Supply item not found!'), 404);
-        }
+        $key = $this->get('id');
+        $this->crud_get($key);
     }
 
-    // Handle an incoming GET ... return 1 supply item
-    function item_get() {
-        $key = $this->get($id);
-        $result = $this->suppliesmodel->get($key);
+    function item_get($key = null) {
+        if (($key == null) || ($key == 'id'))
+            $key = $this->get('id');
+        $this->crud_get($key);
+    }
+
+    private function crud_get($key = null) {
+        if (!$key) {
+            $this->response($this->SuppliesModel->all(), 200);
+            return;
+        }
+
+        $result = $this->SuppliesModel->get($key);
         if ($result != null)
             $this->response($result, 200);
         else
-            $this->response(array('error' => 'Supply item not found!'), 404);
+            $this->response(array('error' => 'Read: Supply item ' . $key . ' not found!'), 404);
     }
 
-    // Handle an incoming POST - add a new menu item
     function index_post() {
-        $key = $this->get('id');
-        $record = array_merge(array('id' => $key), $_POST);
-        $this->suppliesmodel->add($record);
-        $this->response(array('ok'), 200);
+        $this->crud_post($this->post());
     }
 
-    // Handle an incoming POST - add a new menu item
-    function item_post() {
-        $key = $this->get('id');
-        $record = array_merge(array('id' => $key), $_POST);
-        $this->suppliesmodel->add($record);
-        $this->response(array('ok'), 200);
+    function item_post($key = null) {
+        $record = json_decode($this->post(), true);
+
+        if (($key == null) || ($key == 'id')) {
+            $key = $this->get('id');
+            $record = array_merge(array('id' => $key), $record);
+        }
+
+        $this->crud_post($record);
     }
 
-    // Handle an incoming PUT - update a menu item
-    function index_put() {
-        $key = $this->get('id');
-        $record = array_merge(array('id' => $key), $this->_put_args);
-        $this->suppliesmodel->update($record);
-        $this->response(array('ok'), 200);
-    }
+    private function crud_post($record = null) {
+        $key = $record['id'];
 
-    function item_put() {
-        $key = $this->get('id');
-        $record = array_merge(array('id' => $key), $this->_put_args);
-        $this->suppliesmodel->update($record);
-        $this->response(array('ok'), 200);
-    }
+        if (!isset($key)) {
+            $this->response(array('error' => 'Create: No supply specified'), 406);
+            return;
+        }
 
-    // Handle an incoming DELETE - delete a menu item
-    function index_delete() {
-        $key = $this->get('id');
-        $this->suppliesmodel->delete($key);
-        $this->response(array('ok'), 200);
-    }
+        if ($this->SuppliesModel->exists($key)) {
+            $this->response(array('error' => 'Create: Supply ' . $key . ' already exists'), 406);
+            return;
+        }
 
-    function item_delete() {
-        $key = $this->delete('id');
-        $fake = $this->suppliesmodel->delete($key);
-        if ($fake === null)
+        $this->SuppliesModel->add($record);
+
+        $oops = $this->db->error();
+        if (empty($oops['code']))
             $this->response(array('ok'), 200);
+        else
+            $this->response($oops, 400);
+    }
 
-        $this->response(array($fake), 500);
+    function index_put() {
+        $this->crud_put($this->post());
+    }
+
+    function item_put($key = null) {
+        $incoming = key($this->put());
+
+        $record = json_decode($incoming, true);
+
+        if (($key == null) || ($key == 'id')) {
+            $key = $this->get('id');
+            $record = array_merge(array('id' => $key), $record);
+        }
+
+        $this->crud_put($record);
+    }
+
+    private function crud_put($record = null) {
+        $key = $record['id'];
+
+        if (!isset($key)) {
+            $this->response(array('error' => 'Update: No supply specified'), 406);
+            return;
+        }
+
+        if (!$this->SuppliesModel->exists($key)) {
+            $this->response(array('error' => 'Update: Supply ' . $key . ' not found'), 406);
+            return;
+        }
+
+        $this->SuppliesModel->update($record);
+
+        $oops = $this->db->error();
+        if (empty($oops['code']))
+            $this->response(array('ok'), 200);
+        else
+            $this->response($oops, 400);
+    }
+
+    function index_delete() {
+        $this->crud_delete($this->delete());
+    }
+
+    function item_delete($key = null) {
+        if (($key == null) || ($key == 'id')) {
+            $key = $this->get('id');
+        }
+
+        $this->crud_delete($key);
+    }
+
+    private function crud_delete($key = null) {
+        if (!isset($key)) {
+            $this->response(array('error' => 'Delete: No supply specified'), 406);
+            return;
+        }
+
+        if (!$this->SuppliesModel->exists($key)) {
+            $this->response(array('error' => 'Delete: Supply ' . $key . ' not found'), 406);
+            return;
+        }
+
+        $this->SuppliesModel->delete($key);
+        $this->response(array('error' => $this->db->error(),
+            'test' => 'testing'), 500);
+        return;
+
+        $oops = $this->db->error();
+        if (empty($oops['code']))
+            $this->response(array('ok'), 200);
+        else
+            $this->response($oops, 400);
+    }
+
+    function rules_get() {
+        $this->response($this->SuppliesModel->rules(), 200);
+    }
+
+    function create_get() {
+        return $this->response($this->SuppliesModel->create(), 200);
     }
 }
